9eebafebc40fe6046339edbc0999ce52ec19651f
Miguel Casado <miguelantonio96@hotmail.com>
2024-04-29 02:02:13 -0400

    uploading Alura play project - file

---
+++ js/app.js
@@ -0,0 +1,174 @@
+const videosInfo = [
+  {
+    titulo:
+      "Descubre todo sobre el ChatGPT: Descifrando los Secretos de los Grandes",
+    vistas: "456 vistas - hace 2 semanas",
+    src: "https://www.youtube.com/embed/PwheldW6V98?si=mJKUmresNkF_c4Q1",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "CURSO REACT 2024 - Aprende desde cero",
+    vistas: "590 K visualizaciones hace 1 año",
+    src: "https://www.youtube.com/embed/7iobxzd_2wY?si=CcvsbbxuGQwIHni_",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo:
+      "Funcionalidad en foco: introducción a la programación funcional #Aluratips",
+    vistas: "242 vistas - hace 1 mes",
+    src: "https://www.youtube.com/embed/qBrlvcQjrCA?si=NtRiXE2M6UXFpuVy",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Las ventajas del aprendizaje autodidacta #Aluratips",
+    vistas: "312 vistas - hace 1 meses",
+    src: "https://www.youtube.com/embed/xdzz3_WqFmE?si=K3vRJxqY2Xt9v2D4",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Cómo aprender mejor #aluramás",
+    vistas: "242 vistas - hace 2 meses",
+    src: "https://www.youtube.com/embed/DeM2Ak8tWFU?si=z9eTmcwKMhbGnOSM",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "WebScraping y análisis de datos #aluramás",
+    vistas: "266 vistas - hace 2 meses",
+    src: "https://www.youtube.com/embed/b5M9ws0X3FQ?si=BByTwoZV_195ifW0",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Inmersión Dev: Tus primeros pasos con Javascript",
+    vistas: "8.4 k vistas -Transmitido hace 3 mes",
+    src: "https://www.youtube.com/embed/SAusS82-xQ0?si=w0csFllv1LjuLs7O",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Productividad con ChatGPT y Excel #Aluratips",
+    vistas: "459 vistas- hace 3 meses",
+    src: "https://www.youtube.com/embed/hNUuAYmGL1M?si=BLk4jJSDjyTHBoXO",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Consejos para aumentar tu productividad en el trabajo...",
+    vistas: "470 vistas - hace 3 meses",
+    src: "https://www.youtube.com/embed/AULPtYo25Lw?si=Cp7Ovqt-8SBA_3tj",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Camino hacia el éxito como desarrollador Java #Aluratips",
+    vistas: "918 vistas - hace 4 meses",
+    src: "https://www.youtube.com/embed/zOetOoeOrOg?si=R64GU4elwkU-ce7V",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Desarrollo Senior: Descubre el Significado #Aluratips",
+    vistas: "550 vistas - hace 4 meses",
+    src: "https://www.youtube.com/embed/VKj8MlylaPg?si=3nhakwSHEgqM_RL6",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Front-end vs. Back-end #LuchaDev",
+    vistas: "1.3 k vistas - hace 7 meses",
+    src: "https://www.youtube.com/embed/QjOWz9avkg8?si=hLgmCHaYTWoHi03R",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Cómo funciona el Machine Learning #aluramás",
+    vistas: "335 vistas - hace 2 meses",
+    src: "https://www.youtube.com/embed/1EJ69twpnMo?si=77XLahry_zDuc666",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Inmersión Datos 2: ¿Cuáles son los próximos pasos...",
+    vistas: "3.7 k vistas - hace 4 meses",
+    src: "https://www.youtube.com/embed/CpL7-sbW2O0?si=nBPiodanhasOV7D8",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "Cuándo usar let, var y const? #AluraMás",
+    vistas: "69 k de vistas - hace 1 año",
+    src: "https://www.youtube.com/embed/PztCEdIJITY?si=5fxDg327jUp5TA_X",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },
+  {
+    titulo: "¿Qué es la lógica de programación  #AluraMás",
+    vistas: "95 k vistas - hace 1 año",
+    src: "https://www.youtube.com/embed/3W983Q2NzRI?si=eCcE3jEyAUJu_tju",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },{
+    titulo: "Aprende JavaScript Ahora! curso completo desde cero para principiantes",
+    vistas: "250.891 visualizaciones  2 ene 2024",
+    src: "https://www.youtube.com/embed/QoC4RxNIs5M?si=-8bBEwM1JIAoYYNf",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  },{
+    titulo: "Curso de JAVASCRIPT desde CERO (Completo) - Nivel JUNIOR",
+    vistas: "4,3 M de visualizaciones  hace 3 años",
+    src: "https://www.youtube.com/embed/z95mZVUcJ-E?si=FfuUXg9dPDRzuvLe",
+    logo: "./img/menu/video-icon.png",
+    canal: "Alura Latam",
+  }
+];
+
+videosInfo.forEach((videoSection) => {
+  const sections = videosInfo.filter((section) => {
+    return videoSection === section;
+  });
+
+  createVideosElements(sections);
+});
+
+function createVideosElements(videos) {
+  const ulContainer = document.getElementById("videos__container");
+  const li = document.createElement("li");
+  li.classList.add("videos__item");
+
+  videos.map((videos) => {
+    li.innerHTML = `
+            <iframe  width="100%"  height="72%" src="${videos.src}" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="descripcion-video">
+                <img src="${videos.logo}" alt="${videos.canal}">
+                <div class="detalles__video">
+                    <h3>${videos.titulo}</h3>
+                    <p>${videos.vistas}</p>
+                </div>
+            </div>
+        
+`;
+  });
+
+  ulContainer.appendChild(li);
+}
+
+// const search = document.getElementById('buscar');
+// const searchIcon = document.querySelector('.buscar__label');
+
+// console.log(search)
+// search.addEventListener('click', ()=>{
+//   if (search){
+//     search.placeholder = ""
+//   } else{
+//     if (!search){
+//       search.placeholder = "buscar"
+//     }
+//   }
+  
+// })
